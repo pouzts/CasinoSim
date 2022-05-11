cb7c39f140926e295d5ce6be0805e6312f79be30
jchanNeumont <jchan@student.neumont.edu>
2022-05-10 19:53:12 -0600

    Rename Script

--- Assets/Scripts/BankLogic.cs
+++ Assets/Scripts/BankLogic.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TestingScript : MonoBehaviour
+public class BankLogic : Singleton<BankLogic>
 {
     [SerializeField] TMP_Text PlayerBank;
     [SerializeField] InputField WithdrawAmount;
