0a314cb35f8727d38677b01cc4e06f1e11aee5fa
carlosbarajas19 <cbarajas@student.neumont.edu>
2022-05-12 20:20:42 -0600

    c

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -12,27 +12,6 @@ public enum CardSuit
 public class Card : MonoBehaviour
 {
 
-    private CardSuit cardSuit;
-    private int value;
-
-    public CardSuit Suit { get { return cardSuit; } }
-    public int cardValue { get { return value; } }
-
-    public GameObject card;
-
-    public Card(CardSuit suit, int rank, Vector2 position, Quaternion rotation)
-    {
-        string assetName = string.Format("Card_{0}_{1}", suit, rank);
-        GameObject asset = GameObject.Find(assetName);
-        if (asset == null)
-        {
-            Debug.LogError("Asset '" + assetName + "' could not be found.");
-        }
-        else
-        {
-            card = Instantiate(asset, position, rotation);
-            cardSuit = suit;
-            value = rank;
-        }
-    }
+    public CardSuit cardSuit;
+    public int value;
 }
