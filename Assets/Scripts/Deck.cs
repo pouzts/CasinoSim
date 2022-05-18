5a541d0bbe359b640f2dae1d424e0451d8640944
Joey <48295680+Colbalt01@users.noreply.github.com>
2022-05-17 19:58:13 -0600

    PullThing2

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
-    public List<Card> MainDeck = new List<Card>();
-    public List<Card> DiscardPile = new List<Card>();
-    public List<Card> ActiveHand = new List<Card>();
+    public List<GameObject> MainDeck = new List<GameObject>();
+    public List<GameObject> DiscardPile = new List<GameObject>();
+    public List<GameObject> ActiveHand = new List<GameObject>();
 
     public void DrawCard()
     {
@@ -16,7 +16,7 @@ public class Deck : MonoBehaviour
             print("you stupid bum, there ain't no cards");
             return;
         }
-            Card card = MainDeck[cardDraw];
+            var card = MainDeck[cardDraw];
             MainDeck.RemoveAt(cardDraw);
             ActiveHand.Add(card);
     }
@@ -25,9 +25,25 @@ public class Deck : MonoBehaviour
     {
         for (int i = 0; i < ActiveHand.Count; i++)
         {
-            Card card = ActiveHand[i];
+            var card = ActiveHand[i];
             ActiveHand.RemoveAt(i);
             DiscardPile.Add(card);
         }
+    }
+
+    public void ResetDeck()
+    {
+        foreach (var card in DiscardPile)
+        {
+            MainDeck.Add(card);
+            DiscardPile.Remove(card);
+        }
+        foreach (var card in ActiveHand)
+        {
+            MainDeck.Add(card);
+            ActiveHand.Remove(card);
+        }
+        ActiveHand.Clear();
+        DiscardPile.Clear();
     }
 }
