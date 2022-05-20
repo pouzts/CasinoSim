d922f4d2ca2014ebb992d93860d3b0c285686bdd
jchanNeumont <jchan@student.neumont.edu>
2022-05-19 20:04:04 -0600

    Revert "Revert "Merge branch 'main' of https://github.com/pouzts/CasinoSim into main""
    This reverts commit ba907cf1756ae544fefb3ddc4e4800aac7e38186.

--- Assets/Scripts/GameLogic/BlackJackLogic.cs
+++ Assets/Scripts/GameLogic/BlackJackLogic.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BlackJackLogic : Singleton<BlackJackLogic>
 {
     [SerializeField] TMP_Text currentBet;
     [SerializeField] GameObject dealerHandPanel;
     [SerializeField] GameObject playerHandPanel;
-    Deck deck;
+    [SerializeField] Texture faceDownCard;
+
+    public GameObject deck;
 
     List<GameObject> dealerHand = new List<GameObject>();
     List<GameObject> playerHand = new List<GameObject>();
@@ -62,25 +65,36 @@ public class BlackJackLogic : Singleton<BlackJackLogic>
         }*/
     }
 
-    void PlaceBets(int betAmmount)
+    public void PlaceBets()
     {
         //enable chip buttons
         //enable "place bet" button
-        bet += betAmmount;
+
+        //int betAmmount = int.Parse(currentBet.text);
+        //bet += betAmmount;
+
+        DealCards();
+
     }
 
-    void DealCards()
+    public void AddToBet(int value)
     {
-        GameObject card1 = Instantiate(deck.DrawCard(), dealerHandPanel.transform);
-        GameObject card2 = Instantiate(deck.DrawCard(), dealerHandPanel.transform);
+        bet += value;
+        currentBet.text = "$" + bet.ToString();
+    }
 
+    public void DealCards()
+    {
+        GameObject card1 = Instantiate(deck.GetComponent<Deck>().DrawCard(), dealerHandPanel.transform);
+        GameObject card2 = Instantiate(deck.GetComponent<Deck>().DrawCard(), dealerHandPanel.transform);
 
+        card1.GetComponent<RawImage>().texture = faceDownCard;
 
         dealerHand.Add(card1);
         dealerHand.Add(card2);
 
-        card1 = Instantiate(deck.DrawCard(), dealerHandPanel.transform);
-        card2 = Instantiate(deck.DrawCard(), dealerHandPanel.transform);
+        card1 = Instantiate(deck.GetComponent<Deck>().DrawCard(), playerHandPanel.transform);
+        card2 = Instantiate(deck.GetComponent<Deck>().DrawCard(), playerHandPanel.transform);
         dealerHand.Add(card1);
         dealerHand.Add(card2);
     }
