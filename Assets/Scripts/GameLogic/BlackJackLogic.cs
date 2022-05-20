5d9f0c01a62db372a3833257842ada2ca887ee5c
jchanNeumont <jchan@student.neumont.edu>
2022-05-19 19:46:15 -0600

    God Lord Help Me

--- Assets/Scripts/GameLogic/BlackJackLogic.cs
+++ Assets/Scripts/GameLogic/BlackJackLogic.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BlackJackLogic : Singleton<BlackJackLogic>
 {
+    [SerializeField] TMP_Text currentBet;
+    [SerializeField] GameObject dealerHandPanel;
+    [SerializeField] GameObject playerHandPanel;
+    Deck deck;
 
+    List<GameObject> dealerHand = new List<GameObject>();
+    List<GameObject> playerHand = new List<GameObject>();
 
-
+    int playerHandVal;
     int bet;
 
-    eGameState gameState;
+    //eGameState gameState;
 
     public enum eGameState
     {
@@ -23,12 +30,14 @@ public class BlackJackLogic : Singleton<BlackJackLogic>
 
     private void Start()
     {
-        gameState = eGameState.PLACE_BETS;
+        //gameState = eGameState.PLACE_BETS;
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentBet.text = bet.ToString();
+
         /*switch (gameState)
         {
             case eGameState.PLACE_BETS:
@@ -57,12 +66,23 @@ public class BlackJackLogic : Singleton<BlackJackLogic>
     {
         //enable chip buttons
         //enable "place bet" button
+        bet += betAmmount;
     }
 
     void DealCards()
     {
-        
+        GameObject card1 = Instantiate(deck.DrawCard(), dealerHandPanel.transform);
+        GameObject card2 = Instantiate(deck.DrawCard(), dealerHandPanel.transform);
 
+
+
+        dealerHand.Add(card1);
+        dealerHand.Add(card2);
+
+        card1 = Instantiate(deck.DrawCard(), dealerHandPanel.transform);
+        card2 = Instantiate(deck.DrawCard(), dealerHandPanel.transform);
+        dealerHand.Add(card1);
+        dealerHand.Add(card2);
     }
 
     void PlayerTurn()
