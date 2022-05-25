7b866e27f8bdbcda360318ca7997037945d59475
MauricioB1 <morriss3031@gmail.com>
2022-05-24 19:47:00 -0600

    kms );
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa A

--- Assets/Scripts/RouletteButtonScript.cs
+++ Assets/Scripts/RouletteButtonScript.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RouletteButtonScript : MonoBehaviour
 {
     public bool IsTrue = false;
     public int Val;
     public RoulettePieceColor Color;
+    public int betVal;
+
+    public void OnClick()
+    {
+        var tempColor = gameObject.GetComponent<RawImage>().color;
+
+        if (IsTrue)
+        {
+            tempColor.a = 0f;
+            gameObject.GetComponent<RawImage>().color = tempColor;
+            IsTrue = false;
+        }
+            
+        else
+        {
+            tempColor.a = 255f;
+            gameObject.GetComponent<RawImage>().color = tempColor;
+            IsTrue = true;
+        }
+    }
+
 }
 
 
